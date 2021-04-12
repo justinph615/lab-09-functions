e21ee561627a02b060b6f22ef6e56fa184be0cf4
justinph615 <justinph615@gmail.com>
2021-04-12 16:06:44 -0400

    added 4 files

---
+++ greetings.py
@@ -0,0 +1,12 @@
+
+
+def greet():
+
+ print("hello")
+
+
+for i in range(0,5):
+
+ greet()
+
+	
---
+++ pluralize.py
@@ -0,0 +1,9 @@
+def pluralize(word):
+
+
+ return word + "s" 
+
+print("hello")
+
+userword = input("Word please: ")
+print(pluralize(userword)) 
