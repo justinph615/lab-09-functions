ff9fc4cca5c2168e81ed0317ff2d775a8119e82f
justinph615 <justinph615@gmail.com>
2021-04-12 18:35:10 -0400

    added other files

---
+++ double.py
@@ -0,0 +1,12 @@
+def double(n):
+
+	n = int(n)  
+	return n * 2
+
+
+userstring = input("Number please: ")
+#usernum = int(userstring)
+
+print(double(userstring))
+		
+
---
+++ introduce.py
@@ -0,0 +1,9 @@
+def introduce(person1, person2):
+
+  print("hello " + person1 + " this is " + person2 +  ".")
+
+p1 = input("Name a person: ")
+p2 = input("Name another person: ")
+
+introduce(p1, p2)
+
